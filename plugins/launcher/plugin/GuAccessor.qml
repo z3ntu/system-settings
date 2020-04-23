02442f9649f7edc954b94f13514f9c8aaef669ad
Luca Weiss <luca@z3ntu.xyz>
2020-04-23 23:00:45 +0200

    DNM remove stuff 2

--- plugins/launcher/plugin/GuAccessor.qml
+++
@@ -1,6 +0,0 @@
-import QtQml 2.2
-import Ubuntu.Components 1.3
-
-QtObject {
-    property int largeScreenThreshold: units.gu(90)
-}
--- src/main.cpp
+++ src/main.cpp
@@ -23,67 +23,8 @@
 #include "plugin-manager.h"
 #include "utils.h"
 
-#include <QByteArray>
-#include <QApplication>
-#include <QProcessEnvironment>
-#include <QQmlContext>
-#include <QUrl>
-#include <QQuickView>
-#include <QtGlobal>
-#include <QtQml>
-#include <QtQml/QQmlDebuggingEnabler>
-static QQmlDebuggingEnabler debuggingEnabler(false);
-
-using namespace SystemSettings;
+//static QQmlDebuggingEnabler debuggingEnabler(false);
 
 int main(int argc, char **argv)
 {
-    QApplication app(argc, argv);
-    QByteArray mountPoint = qEnvironmentVariableIsSet("SNAP") ? qgetenv("SNAP") : "";
-    bool isSnap = !mountPoint.isEmpty();
-
-    // Ensure printing environment is correct.
-    qputenv("QT_PRINTER_MODULE", "cupsprintersupport");
-
-    /* read environment variables */
-    QProcessEnvironment environment = QProcessEnvironment::systemEnvironment();
-    if (environment.contains(QLatin1String("SS_LOGGING_LEVEL"))) {
-        bool isOk;
-        int value = environment.value(
-            QLatin1String("SS_LOGGING_LEVEL")).toInt(&isOk);
-        if (isOk)
-            setLoggingLevel(value);
-    }
-
-    initTr(I18N_DOMAIN, nullptr);
-    /* HACK: force the theme until lp #1098578 is fixed */
-    QIcon::setThemeName("suru");
-
-    /* Parse the commandline options to see if we've been given a panel to load,
-     * and other options for the panel.
-     */
-    QString defaultPlugin;
-    QVariantMap pluginOptions;
-    parsePluginOptions(app.arguments(), defaultPlugin, pluginOptions);
-
-    QQuickView view;
-    Utilities utils;
-    QObject::connect(view.engine(), SIGNAL(quit()), &app, SLOT(quit()),
-                     Qt::QueuedConnection);
-    qmlRegisterType<QAbstractItemModel>();
-    qmlRegisterType<SystemSettings::PluginManager>("SystemSettings", 1, 0, "PluginManager");
-    view.engine()->rootContext()->setContextProperty("Utilities", &utils);
-    view.setResizeMode(QQuickView::SizeRootObjectToView);
-    view.engine()->addImportPath(mountPoint + PLUGIN_PRIVATE_MODULE_DIR);
-    view.engine()->addImportPath(mountPoint + PLUGIN_QML_DIR);
-    view.rootContext()->setContextProperty("defaultPlugin", defaultPlugin);
-    view.rootContext()->setContextProperty("mountPoint", mountPoint);
-    view.rootContext()->setContextProperty("isSnap", isSnap);
-    view.rootContext()->setContextProperty("i18nDirectory", mountPoint + I18N_DIRECTORY);
-    view.rootContext()->setContextProperty("pluginOptions", pluginOptions);
-    view.rootContext()->setContextProperty("view", &view);
-    view.setSource(QUrl("qrc:/qml/MainWindow.qml"));
-    view.show();
-
-    return app.exec();
 }
